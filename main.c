b07b2f5d4dab6ca80a9e747429501d0ad52d1677
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2016-06-24 13:03:20 -0400

    Don't need malloc to trigger the issue

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    char *blah = (char *) malloc(100);
-    if (blah) {
-        strcpy(blah, "Hello world!");
-        printf("%s\n", blah);
-        free(blah);
-    }
     return EXIT_SUCCESS;
 }
